42e02a9a1b4d27d0657f2419d367a966564f6bac
Mirna Yacout <mirna.yacout@gmail.com>
2015-04-14 17:08:14 +0200

    root_url problem solved

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,7 +4,7 @@ class GamesController < ApplicationController
 
 	def index
 		if user_signed_in?
-			redirect_to controller: "games", action: "home"
+			return redirect_to controller: "games", action: "home"
 		end
 	end
 
